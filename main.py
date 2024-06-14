d174864ccda785596819ae7fe71ce1908858b1a2
Shreyas Rai <shreyasrai23@gmail.com>
2024-06-14 13:09:52 -0700

    did some restructuring and added an alternative path to the pipeline

---
+++ main.py
@@ -0,0 +1,43 @@
+import prototype
+import rag
+import repair_plan
+import json
+
+
+
+
+def initial_diagnosis(image_url, user_msg):
+    initial_prompt, secondary_prompt, additional_questions = prototype.get_prompts()
+    return prototype.process_image("gpt-4o", initial_prompt, user_msg, image_url)
+
+def secondary_diagnosis(image_url, init_diag):
+    initial_prompt, secondary_prompt, additional_questions = prototype.get_prompts()
+    return prototype.ask_additional_questions("gpt-4o", image_url, init_diag, secondary_prompt, additional_questions)
+
+def create_diagnostic_report(input_query):
+    return rag.main(input_query)
+
+def create_repair_plan(init_diagnosis, context, query):
+    return repair_plan.generate_repair_plan(init_diagnosis, context, query)
+
+
+IMAGE_URL = "https://drive.google.com/uc?export=download&id=17yL8_Ml4KShHQOJv8MlzHCmeWVcXMYoY"
+USER_MESSAGE = "This is a Philips Ultrasound Machine, it's a CX50. The screen is cracked and the machine is not turning on."
+MODEL = "gpt-4o"
+
+# Steps:
+
+init_diagnosis = initial_diagnosis(IMAGE_URL, USER_MESSAGE)
+
+secondary_diagnosis = secondary_diagnosis(IMAGE_URL, init_diagnosis)
+
+if json.loads(secondary_diagnosis)["remotely_solvable"] == True:
+    print(create_diagnostic_report(secondary_diagnosis))
+
+elif json.loads(secondary_diagnosis)["remotely_solvable"] == False:
+    print(create_repair_plan(init_diagnosis, repair_plan.FLOWCHART_URL, repair_plan.QUESTION))  
+
+else:
+    print("No issues found.")
+
+
---
+++ repair_plan.py
@@ -0,0 +1,24 @@
+import json
+import prototype
+from openai import OpenAI
+
+FLOWCHART_URL = "https://bmet.ewh.org/server/api/core/bitstreams/ac88d555-d4ba-4950-9250-1f4bfb9a9556/content"
+QUESTION = "Based on this initial diagnosis, and the fact that this issue is not remotely solvable, create a repair plan, orders parts needed, and create an incident report. please provide the repair plan based on this flowchart:"
+
+with open("config.json", "r") as config_file:
+    config = json.load(config_file)
+client = OpenAI(api_key=config["openai"]["api_key"])
+
+def generate_repair_plan(init_diagnosis, context, query):
+    """
+    Generates a repair plan based on the initial diagnosis and context (provided flowchart).
+    """
+    prompt = f"{init_diagnosis} \n {query} {context}"
+    completion = client.chat.completions.create(
+        model='gpt-4o',
+        messages=[
+            {"role": "system", "content": prompt},
+        ],
+        temperature=0.0
+    )
+    return completion.choices[0].message.content
